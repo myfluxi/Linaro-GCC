107492018dcacf77e8a5fbf3a962493317528f60
myfluxi <mvdw@arcor.de>
2012-04-04 01:14:48 +0200

    Update to binutils-2.22 and newlib-1.20.0

--- lib/gcc/arm-eabi/4.6.3/plugin/include/configargs.h
+++ lib/gcc/arm-eabi/4.6.3/plugin/include/configargs.h
@@ -1,5 +1,5 @@
 /* Generated automatically. */
-static const char configuration_arguments[] = "/home/fluxi/android/xxChain/source/gcc-linaro-4.6-2012.03/configure --target=arm-eabi --with-mode=thumb --with-arch=armv7-a --with-tune=cortex-a9 --with-fpu=neon --with-float=hard --prefix=/home/fluxi/android/xxChain/linaroXX-4.6-2012.03-neon --with-pkgversion=Linaro-A9-XX.2012.03 --with-gcc --with-gnu-ld --with-gnu-as --disable-nls --enable-languages=c,c++ --with-newlib --with-headers=/home/fluxi/android/xxChain/source/newlib-1.19.0/newlib/libc/include";
+static const char configuration_arguments[] = "/home/fluxi/android/xxChain/source/gcc-linaro-4.6-2012.03/configure --target=arm-eabi --with-mode=thumb --with-arch=armv7-a --with-tune=cortex-a9 --with-fpu=neon --with-float=hard --prefix=/home/fluxi/android/xxChain/linaroXX-4.6-2012.03-neon-DEV --with-pkgversion=Linaro-A9-XX.2012.03 --with-gcc --with-gnu-ld --with-gnu-as --disable-nls --enable-languages=c,c++ --with-newlib --with-headers=/home/fluxi/android/xxChain/source/newlib-1.20.0/newlib/libc/include";
 static const char thread_model[] = "single";
 
 static const struct {
