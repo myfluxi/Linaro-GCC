e6d762f366c58d439d91b096d5798d9796b3fbe7
myfluxi <linflux@arcor.de>
2014-01-29 22:03:17 +0100

    Default to arm instead of thumb, update binutils to 2.24

--- lib/gcc/arm-eabi/4.7.4/plugin/include/configargs.h
+++ lib/gcc/arm-eabi/4.7.4/plugin/include/configargs.h
@@ -1,7 +1,7 @@
 /* Generated automatically. */
-static const char configuration_arguments[] = "/home/fluxi/android/toolchains/source/gcc-linaro-4.7-2014.01/configure --target=arm-eabi --with-mode=thumb --with-arch=armv7-a --with-tune=cortex-a15 --with-fpu=neon --with-float=hard --prefix=/home/fluxi/android/toolchains/linaroXX-4.7-2014.01 --with-pkgversion=Linaro-Krait-fluXXi.2014.01 --with-gcc --with-gnu-ld --with-gnu-as --disable-nls --enable-languages=c,c++ --with-newlib --with-headers=/home/fluxi/android/toolchains/source/newlib-2.0.0/newlib/libc/include";
+static const char configuration_arguments[] = "/home/fluxi/android/toolchains/source/gcc-linaro-4.7-2014.01/configure --target=arm-eabi --with-arch=armv7-a --with-cpu=cortex-a9 --with-mode=arm --with-tune=cortex-a15 --with-fpu=neon --with-float=hard --prefix=/home/fluxi/android/toolchains/linaroXX-4.7-2014.01 --with-pkgversion=Linaro-Krait-fluXXi.2014.01 --with-gcc --with-gnu-ld --with-gnu-as --disable-nls --enable-languages=c,c++ --with-newlib --with-headers=/home/fluxi/android/toolchains/source/newlib-2.0.0/newlib/libc/include";
 static const char thread_model[] = "single";
 
 static const struct {
   const char *name, *value;
-} configure_default_options[] = { { "arch", "armv7-a" }, { "tune", "cortex-a15" }, { "float", "hard" }, { "mode", "thumb" }, { "fpu", "neon" } };
+} configure_default_options[] = { { "cpu", "cortex-a9" }, { "arch", "armv7-a" }, { "tune", "cortex-a15" }, { "float", "hard" }, { "mode", "arm" }, { "fpu", "neon" } };
