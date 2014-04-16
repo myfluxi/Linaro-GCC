03a1caf112b61fa21ce6dc3e2a0f4450944fd004
myfluxi <linflux@arcor.de>
2014-04-16 22:57:01 +0200

    Update to Linaro GCC 4.7-2014.04

--- arm-eabi/lib/ldscripts/armelf.xbn
+++ arm-eabi/lib/ldscripts/armelf.xbn
@@ -3,7 +3,7 @@ OUTPUT_FORMAT("elf32-littlearm", "elf32-bigarm",
 	      "elf32-littlearm")
 OUTPUT_ARCH(arm)
 ENTRY(_start)
-SEARCH_DIR("/home/fluxi/android/toolchains/linaroXX-4.7-2014.01/arm-eabi/lib");
+SEARCH_DIR("/home/fluxi/android/toolchains/linaroXX-4.7-2014.04/arm-eabi/lib");
 SECTIONS
 {
   /* Read-only sections, merged into text segment: */
--- lib/gcc/arm-eabi/4.7.4/plugin/include/configargs.h
+++ lib/gcc/arm-eabi/4.7.4/plugin/include/configargs.h
@@ -1,5 +1,5 @@
 /* Generated automatically. */
-static const char configuration_arguments[] = "/home/fluxi/android/toolchains/source/gcc-linaro-4.7-2014.01/configure --target=arm-eabi --with-arch=armv7-a --with-cpu=cortex-a9 --with-mode=arm --with-tune=cortex-a15 --with-fpu=neon --with-float=hard --prefix=/home/fluxi/android/toolchains/linaroXX-4.7-2014.01 --with-pkgversion=Linaro-Krait-fluXXi.2014.01 --with-gcc --with-gnu-ld --with-gnu-as --disable-nls --enable-languages=c,c++ --with-newlib --with-headers=/home/fluxi/android/toolchains/source/newlib-2.0.0/newlib/libc/include";
+static const char configuration_arguments[] = "/home/fluxi/android/toolchains/source/gcc-linaro-4.7-2014.04/configure --target=arm-eabi --with-arch=armv7-a --with-cpu=cortex-a9 --with-mode=arm --with-tune=cortex-a15 --with-fpu=neon --with-float=hard --prefix=/home/fluxi/android/toolchains/linaroXX-4.7-2014.04 --with-pkgversion=Linaro-Krait-fluXXi.2014.04 --with-gcc --with-gnu-ld --with-gnu-as --disable-nls --enable-languages=c,c++ --with-newlib --with-headers=/home/fluxi/android/toolchains/source/newlib-2.0.0/newlib/libc/include";
 static const char thread_model[] = "single";
 
 static const struct {
